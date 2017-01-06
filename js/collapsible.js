f4bf314a40e7d9d9fdfcf4ec02210c490af00c7c
Shun Lin <s92014721@berkeley.edu>
2017-01-06 15:35:40 -0800

    testing

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-   $('.collapsible').collapsible();
+   $('.collapsible').collapsible({
+       accordion: true, // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+   });
 });
