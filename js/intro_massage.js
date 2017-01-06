dff5385998d7de5e7d34c71ccdfde9c5d65f2b06
Shun Lin <s92014721@berkeley.edu>
2017-01-05 18:23:18 -0800

    minor update

--- js/intro_massage.js
+++ js/intro_massage.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     // updating website message
-    Materialize.toast('Welcome to our new website, some contents are being updated!', 4000);
+    Materialize.toast('Welcome to our new website, more information coming soon!', 4000);
 });
