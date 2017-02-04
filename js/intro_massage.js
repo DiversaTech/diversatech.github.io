bb4b73c37057798650ab0ef7f696db6df236cc4d
Shun Lin <s92014721@berkeley.edu>
2017-02-04 07:38:02 -0800

    update intro

--- js/intro_massage.js
+++ js/intro_massage.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     // updating website message
-    Materialize.toast('Welcome to DiversaTech Consulting, Spring 2017 Application is now open!', 4000);
+    Materialize.toast('Welcome to DiversaTech Consulting!', 2000);
 });
