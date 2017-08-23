6f1842dda3da228ee13d5b3af084d202c6a7842b
Shun Lin <s92014721@berkeley.edu>
2017-08-22 19:23:26 -0700

    update intro message

--- js/intro_massage.js
+++ js/intro_massage.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     // updating website message
-    Materialize.toast('Fall 2018 Update coming soon!', 4000);
+    Materialize.toast('Welcome to DiversaTech!', 4000);
 });
