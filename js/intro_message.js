33377d7eda5e9f446b7f6aa412b6436141dae92e
Shun Lin <s92014721@berkeley.edu>
2017-09-14 10:41:39 -0700

    take away intro and apply messages

--- js/intro_message.js
+++ js/intro_message.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     // updating website message
-    Materialize.toast('Fall 2017 Application is now closed.', 4000);
+    // Materialize.toast('Fall 2017 Application is now closed.', 4000);
 });
