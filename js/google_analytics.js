cb7f29302496533cffcadb146f871bd954090694
Shun Lin <s92014721@berkeley.edu>
2017-10-14 13:51:42 -0700

    added google optimize code

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-105153165-1', 'auto');
+ga('require', 'GTM-WX5BQDV');
 ga('send', 'pageview');
