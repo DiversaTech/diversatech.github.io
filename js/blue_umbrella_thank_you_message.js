0176c7fc1c549705fe35c179ddcc5a013028b81a
cooliotonyio <tony.ytzhao@gmail.com>
2018-08-22 18:44:45 -0700

    removed blue umbrella stuff

--- js/blue_umbrella_thank_you_message.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    // updating website message
-    Materialize.toast('Thank you for applying for Blue Umbrella Fall 2017 Part-Time Internship program, we will contact you soon!', 10000);
-});
